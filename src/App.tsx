09f8a624132b7b5ad065bc98545768e80eaedbbf
Denzel <denzeltlee@gmail.com>
2021-05-25 20:07:50 +0800

    Init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Typography } from "@material-ui/core";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const useStyles = makeStyles((theme) => ({
+    root: {},
+    main: {},
+    headerTitle: {
+        textAlign: "center",
+        padding: "3rem 0",
+    },
+}));
+
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <main className={classes.main}>
+                <Typography variant="h3" component="h1" className={classes.headerTitle}>
+                    Notes App
+                </Typography>
+            </main>
+        </div>
+    );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider } from "@material-ui/styles";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: "#267fa6",
+        },
+        secondary: {
+            main: "#eb7628",
+        },
+        success: {
+            main: "#42b347",
+        },
+        error: {
+            main: "#ef4a4a",
+        },
+        background: {
+            default: "#ffffff",
+        },
+    },
+    typography: {
+        fontFamily: ["Lato", "Roboto", "Helvetica", "Arial", "sans-serif"].join(","),
+    },
+    breakpoints: {
+        values: {
+            xs: 0,
+            sm: 480,
+            md: 768,
+            lg: 992,
+            xl: 1200,
+        },
+    },
+    overrides: {
+        MuiButton: {
+            root: {
+                textTransform: "capitalize",
+            },
+        },
+    },
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>,
+    document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
